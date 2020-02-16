57992dc1dd775752409789b7424396eb2a15d073
Alex <a.french@mail.utoronto.ca>
2020-02-16 15:36:30 -0500

    Update navigation bar

--- app/src/components/Navigation.jsx
+++ app/src/components/Navigation.jsx
@@ -4,34 +4,137 @@
  */
 
 import React from "react";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import {
-    AppBar, IconButton, Toolbar, Typography,
+    AppBar, IconButton, Menu, MenuItem, Toolbar, Typography,
 } from "@material-ui/core";
-import { AccountCircle } from "@material-ui/icons";
+import {
+    AccountCircle, DirectionsRun, Home, People, Today,
+} from "@material-ui/icons";
 
-const _Navigation = ({ user }) => (
-    <AppBar position="static">
-        <Toolbar>
-            <Typography variant="h5">
-                FitPro
-            </Typography>
-            <div className="navbar-right">
-                {user != null
-            && (
-                <div>
-                    <IconButton>
-                        <AccountCircle />
-                        <Typography className="navbar-name">
-                            {user.firstname}
+function _Navigation({ user }) {
+    const [userMenuAnchorEl, setUserMenuAnchorEl] = React.useState(null);
+    const isOpen = Boolean(userMenuAnchorEl);
+    const menuOpen = (event) => setUserMenuAnchorEl(event.currentTarget);
+    const menuClose = () => setUserMenuAnchorEl(null);
+
+    return (
+        <AppBar position="static" className="navbar-container">
+            <Toolbar>
+                <div className="navbar-left">
+                    <Link to="/" className="navbar-link">
+                        <Typography variant="h5">
+                            FitPro
                         </Typography>
-                    </IconButton>
+                    </Link>
                 </div>
-            )}
-            </div>
-        </Toolbar>
-    </AppBar>
-);
+
+                <div className="navbar-center">
+                    <div className="navbar-always">
+                        <Link to="/" className="navbar-link">
+                            <IconButton>
+                                <Home />
+                                <Typography className="navbar-label">
+                                    Home
+                                </Typography>
+                            </IconButton>
+                        </Link>
+                        <Link to="/exercises" className="navbar-link">
+                            <IconButton>
+                                <DirectionsRun />
+                                <Typography className="navbar-label">
+                                    Exercises
+                                </Typography>
+                            </IconButton>
+                        </Link>
+                    </div>
+                    {user.isTrainer
+                        && (
+                            <div className="navbar-trainer">
+                                <Link to="/clients" className="navbar-link">
+                                    <IconButton>
+                                        <People />
+                                        <Typography className="navbar-label">
+                                            Clients
+                                        </Typography>
+                                    </IconButton>
+                                </Link>
+                            </div>
+                        )}
+                    {!user.isTrainer
+                        && (
+                            <div className="navbar-client">
+                                <Link to="/trainers" className="navbar-link">
+                                    <IconButton>
+                                        <People />
+                                        <Typography className="navbar-label">
+                                            Trainers
+                                        </Typography>
+                                    </IconButton>
+                                </Link>
+                            </div>
+                        )}
+                    {user != null && (
+                        <div className="navbar-logged-in">
+                            <Link to="/calendar" className="navbar-link">
+                                <IconButton>
+                                    <Today />
+                                    <Typography className="navbar-label">
+                                        Calendar
+                                    </Typography>
+                                </IconButton>
+                            </Link>
+                        </div>
+                    )}
+                </div>
+
+                <div className="navbar-right">
+                    {user == null && (
+                        <Link to="/register" className="navbar-link">
+                            <div className="navbar-logged-out">
+                                <Typography>Sign Up</Typography>
+                            </div>
+                        </Link>
+                    )}
+                    {user != null && (
+                        <div className="navbar-logged-in">
+                            <IconButton aria-controls="navbar-user-menu" aria-haspopup="true" onClick={menuOpen}>
+                                <AccountCircle />
+                                <Typography className="navbar-label">
+                                    {user.firstname}
+                                </Typography>
+                            </IconButton>
+                            <Menu
+                                id="navbar-user-menu"
+                                anchorEl={userMenuAnchorEl}
+                                getContentAnchorEl={null} // Vertical positioning doesn't work without this
+                                anchorOrigin={{
+                                    vertical: "bottom",
+                                    horizontal: "center",
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: "top",
+                                    horizontal: "center",
+                                }}
+                                open={isOpen}
+                                onClose={menuClose}
+                            >
+                                <Link to="/me" className="navbar-menu-link">
+                                    <MenuItem onClick={menuClose}>Profile</MenuItem>
+                                </Link>
+                                <Link to="/logout" className="navbar-menu-link">
+                                    <MenuItem onClick={menuClose}>Sign Out</MenuItem>
+                                </Link>
+                            </Menu>
+                        </div>
+                    )}
+                </div>
+            </Toolbar>
+        </AppBar>
+    );
+}
 
 const mapStateToProps = (state) => ({ user: state.user });
 
