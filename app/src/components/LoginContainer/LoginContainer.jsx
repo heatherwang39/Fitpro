60f1e04f84d769886f4e1d8c416e2fee8e1d459e
Tom Shen <tomshen0201@gmail.com>
2020-02-26 11:03:09 -0500

    Migrate the jsx code to pure component in loginContainer.

--- app/src/components/LoginContainer/LoginContainer.jsx
+++ app/src/components/LoginContainer/LoginContainer.jsx
@@ -1,7 +1,3 @@
-import {
-    TextField, Button,
-} from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import React, { useState } from "react";
 import PropTypes from "prop-types";
@@ -11,41 +7,12 @@ import {
 import API from "../../api";
 import { loginUser, loginSuccess, loginFailure } from "../../actions/authActions";
 import { gotUserInfo } from "../../actions/userActions";
+import LoginComponent from "./LoginComponent";
 
 const checkLoginSuccess = (response) => response.status === "success";
 
-const styles = {
-    textField: {
-        marginTop: "20px",
-        width: 300,
-    },
-    button: {
-        backgroundColor: "#4786FF",
-        color: "white",
-        marginTop: "20px",
-        float: "right",
-    },
-    loginContainer: {
-        width: 750,
-        height: 550,
-        margin: "auto",
-        paddingTop: "87px",
-    },
-    registerButton: {
-        color: "#5D8FFC",
-        marginTop: 20,
-        fontSize: 11,
-    },
-    loginForm: {
-        width: 300,
-        height: 229,
-        marginLeft: 131,
-    },
-};
-
 const LoginContainer = (props) => {
     // TODO validate username/password
-    const { classes } = props;
     const userInfo = (username, password) => ({ username, password });
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -83,32 +50,13 @@ const LoginContainer = (props) => {
         setPassword(newPassword);
     };
 
-    const register = () => {
-        history.push("/register");
-    };
-
     return (
-        <div className={classes.loginContainer}>
-            <form className={classes.loginForm}>
-                <TextField
-                    id="outlined-basic"
-                    label="Username"
-                    variant="outlined"
-                    className={classes.textField}
-                    onChange={updateUsername}
-                />
-                <TextField
-                    id="outlined-password-input"
-                    type="password"
-                    label="Password"
-                    variant="outlined"
-                    className={classes.textField}
-                    onChange={updatePassword}
-                />
-                <Button className={classes.registerButton} onClick={register}>Register</Button>
-                <Button variant="contained" onClick={login} className={classes.button}>Login</Button>
-            </form>
-        </div>
+        <LoginComponent
+            loginUser={login}
+            updateUsername={updateUsername}
+            updatePassword={updatePassword}
+            linkToRegister={() => { history.push("/register"); }}
+        />
     );
 };
 
@@ -129,7 +77,6 @@ LoginContainer.propTypes = {
     loginSuccess: PropTypes.func.isRequired,
     gotUserInfo: PropTypes.func.isRequired,
     loginFailure: PropTypes.func.isRequired,
-    classes: PropTypes.objectOf(PropTypes.string).isRequired,
 };
 
-export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(LoginContainer));
+export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer);
