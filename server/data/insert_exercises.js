69eb39a7108765c80d115e7207c9b4213ec610a6
Alex <a.french@mail.utoronto.ca>
2020-03-20 17:56:56 -0400

    Add auto dependency installation to insert_exercises

--- server/data/insert_exercises.js
+++ server/data/insert_exercises.js
@@ -19,7 +19,13 @@ const db = mongoose.connection;
 const insertExercises = () => {
     if (!jsonExists || !dbConnected) return;
     console.log("Loading exercises...");
-    const exercises = JSON.parse(fs.readFileSync("bbcom_exercises.json"));
+    let exercises;
+    try {
+        exercises = JSON.parse(fs.readFileSync("bbcom_exercises.json"));
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
     console.log(`Inserting ${exercises.length} exercises...`);
     Exercise.insertMany(exercises, () => {
         const rmExercises = readlineSync.question("Done inserting. Delete json? [y/N]");
@@ -35,12 +41,7 @@ const insertExercises = () => {
     });
 };
 
-if (!fs.existsSync("bbcom_exercises.json")) {
-    const getExercises = readlineSync.question("Exercises database doesn't exist. Download now? [Y/n]");
-    if (getExercises.toLowerCase().includes("n")) {
-        process.exit();
-    }
-    console.log("");
+const downloadExercises = () => {
     const download = spawn("python3", ["./bbcom.py"], { cwd: process.cwd(), stdio: "inherit" });
     download.on("exit", (code) => {
         if (code) {
@@ -54,11 +55,38 @@ if (!fs.existsSync("bbcom_exercises.json")) {
         jsonExists = true;
         insertExercises();
     });
+};
+
+if (!fs.existsSync("bbcom_exercises.json")) {
+    const getExercises = readlineSync.question("Exercises database doesn't exist. Download now? [Y/n] ");
+    if (getExercises.toLowerCase().includes("n")) {
+        process.exit();
+    }
+    console.log("");
+    const requirements = spawn("python3", ["-m", "pip", "install", "-r", "requirements.txt"]);
+    requirements.on("exit", (code) => {
+        if (!code) {
+            downloadExercises();
+            return;
+        }
+        const retry = readlineSync.question("Failed to install requirements for fetching data. Try again with sudo? [Y/n] ");
+        if (retry.toLowerCase().includes("n")) {
+            process.exit();
+        }
+        const sudoRequirements = spawn("sudo", ["python3", "-m", "pip", "install", "-r", "requirements.txt"]);
+        sudoRequirements.on("exit", (retryCode) => {
+            if (retryCode) {
+                console.log("Failed to install requirements with sudo. Try manually installing requirements.txt or using a virtual environment");
+                process.exit(1);
+            }
+            downloadExercises();
+        });
+    });
 }
 
 db.on("error", (e) => {
     console.log(e, "\nError connecting to database, exiting");
-    process.exit();
+    process.exit(1);
 });
 
 db.on("open", () => {
