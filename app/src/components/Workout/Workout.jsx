5b752dbe45bd31e11eca7d256fb4dbeb641a2e22
Alex <a.french@mail.utoronto.ca>
2020-04-04 20:28:29 -0400

    Fix being able to rate people who arent your trainer

--- app/src/components/Workout/Workout.jsx
+++ app/src/components/Workout/Workout.jsx
@@ -150,8 +150,8 @@ const WorkoutComponent = ({
                 {workout.description}
             </Grid.Row>
             <Grid.Row id="workout-rating">
-                {workout.rating || workout.rating === 0 ? (
-                    `${~~(workout.rating * 10 / workout.numRatings)}% liked`
+                {workout.numRatings > 0 ? (
+                    `${~~(workout.rating * 10 / workout.numRatings)}% liked (of ${workout.numRatings})`
                 ) : "No Ratings Yet"}
             </Grid.Row>
             <Grid.Row id="workout-level">
@@ -188,7 +188,7 @@ const WorkoutComponent = ({
             setNewExercises({});
             setWorkout(res.workout);
             if (isNew) {
-                // TODO go to new url
+                location.href = `/workout/${res.workout._id}`;
             }
         };
         if (isNew) {
