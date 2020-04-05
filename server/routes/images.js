995396acbb0eff8e053e4d2abf4f5727dde56d71
Alex <a.french@mail.utoronto.ca>
2020-04-04 21:38:20 -0400

    Add profile picture support

--- server/routes/images.js
+++ server/routes/images.js
@@ -5,31 +5,31 @@
 const express = require("express");
 
 const router = express.Router();
-const { Image } = require("../models/image");
 
 // multipart middleware: access uploaded file from req.file
-const multipart = require('connect-multiparty');
+const multipart = require("connect-multiparty");
+
 const multipartMiddleware = multipart();
 
 // cloudinary: configure using credentials on Cloudinary Dashboard
-const cloudinary = require('cloudinary');
+const cloudinary = require("cloudinary");
+const { Image } = require("../models/image");
+
 cloudinary.config({
-    cloud_name: 'fitpro',
-    api_key: '789846423899437',
-    api_secret: 'CZUz0umiRBwpR_gmTqcdaQksYvQ'
+    cloud_name: "fitpro",
+    api_key: "789846423899437",
+    api_secret: "CZUz0umiRBwpR_gmTqcdaQksYvQ",
 });
 
 
 // a POST route to *create* an image
 router.post("/", multipartMiddleware, (req, res) => {
-    //console.log(req.files)
     // Use uploader.upload API to upload image to cloudinary server.
     cloudinary.uploader.upload(
         req.files.image.path, // req.files contains uploaded files
-        function (result) {
-
+        (result) => {
             // Create a new image using the Image mongoose model
-            var img = new Image({
+            const img = new Image({
                 image_id: result.public_id, // image id on cloudinary server
                 image_url: result.url, // image url on cloudinary server
                 created_at: new Date(),
@@ -37,46 +37,46 @@ router.post("/", multipartMiddleware, (req, res) => {
 
             // Save image to the database
             img.save().then(
-                saveRes => {
-                    res.send(saveRes);
+                () => {
+                    res.status(200).send(img.image_url);
                 },
-                error => {
+                (error) => {
                     res.status(400).send(error); // 400 for bad request
-                }
+                },
             );
-        });
+        },
+    );
 });
 
 // a GET route to get all images
 router.get("/", (req, res) => {
     Image.find().then(
-        images => {
+        (images) => {
             res.send({ images }); // can wrap in object if want to add more properties
         },
-        error => {
+        (error) => {
             res.status(500).send(error); // server error
-        }
+        },
     );
 });
 
-/// a DELETE route to remove an image by its id.
+// / a DELETE route to remove an image by its id.
 router.delete("/:imageId", (req, res) => {
     const imageId = req.params.imageId;
 
     // Delete an image by its id (NOT the database ID, but its id on the cloudinary server)
     // on the cloudinary server
-    cloudinary.uploader.destroy(imageId, function (result) {
-
+    cloudinary.uploader.destroy(imageId, (result) => {
         // Delete the image from the database
         Image.findOneAndRemove({ image_id: imageId })
-            .then(img => {
+            .then((img) => {
                 if (!img) {
                     res.status(404).send();
                 } else {
                     res.send(img);
                 }
             })
-            .catch(error => {
+            .catch((error) => {
                 res.status(500).send(); // server error, could not delete.
             });
     });
